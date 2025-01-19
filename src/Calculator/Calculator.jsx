2877087d7ac680dfed5ce0e6c83ff4727cb1fb3c
Weverson Godinho <weverson.godinho@icloud.com>
2025-01-19 20:14:54 +0000

    add input and label

--- src/Calculator/Calculator.jsx
+++ src/Calculator/Calculator.jsx
@@ -2,7 +2,27 @@ export default function Calculator(){
     
     return(
         <form className="calculator-form">
-
+            <h2>Enter your details below</h2>
+            <div className="calculator-radio">
+                <div className="calculator-metric">
+                    <input type="radio" name="metric" id="metric"/>
+                    <label htmlFor="metric">Metric</label>
+                </div>
+                <div className="calculator-imperial">
+                    <input type="radio" name="imperial" id="imperial"/>
+                    <label htmlFor="imperial">Imperial</label>
+                </div>
+            </div>
+            <div className="calculator-data-in">
+                <div className="calculator-height">
+                    <label htmlFor="height">Height</label>
+                    <input type="number" name="height"/>
+                </div>
+                <div className="calculator-weight">
+                    <label htmlFor="weight">Weight</label>
+                    <input type="number" name="weight"/>
+                </div>
+            </div>
         </form>
     )
 }
